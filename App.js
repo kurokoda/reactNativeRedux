60d3c4a16630966a9aa6199dc792bd43b849198a
Kurokoda <indigo.developer@gmail.com>
2019-01-09 18:49:10 -0500

    Adds header

--- App.js
+++ App.js
@@ -3,12 +3,13 @@ import { View, Text } from "react-native";
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './src/reducers'
+import { Header } from './src/components/common/Header'
 
 const App = () => {
   return (
       <Provider store={createStore(reducers)}>
         <View>
-            <Text>App</Text>
+          <Header text="React Redux" />
         </View>
       </Provider>
   );
