0c9a24dd61720d145c45eb37efde562eafbba106
Kurokoda <indigo.developer@gmail.com>
2019-01-14 18:38:24 -0500

    Implements ListItem

--- src/components/LibraryList/index.js
+++ src/components/LibraryList/index.js
@@ -1,21 +1,22 @@
 import React, { Component } from "React";
 import { FlatList } from "react-native";
 import { connect } from "react-redux";
+import ListItem from '../ListItem'
 
 class LibraryList extends Component {
 
   renderItem(library){
-
+    return <ListItem library={library} />
   }
 
   render() {
-    return {
+    return (
       <FlatList
         data={this.props.libraries}
         renderItem={this.renderItem}
-        keyExtractor={(library)=> library.id}
+        keyExtractor={library => String(library.id)}
       />
-    }
+    )
   }
 }
 
---
+++ src/components/ListItem/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "React";
+
+class ListItem extends Component {
+  render() {
+
+  }
+}
+
+export default ListItem;
